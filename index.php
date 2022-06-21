42ddd7743159540b905030cde1a84021117e3052
MEMiceli <marcello.emilio.miceli@gmail.com>
2022-06-21 16:30:08 +0200

    done?

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php
+$paragrafo = "Dio non esiste, Dio non è causa dei tuoi problemi";
+
+$badword = "Dio";
+$newParagrafo = str_replace($badword, "***",$paragrafo);
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,6 +15,9 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <h1> <?php echo $paragrafo; ?></h1>
+    <h3>lunghezza: <?= strlen($paragrafo)?></h3>
+    <h1> <?php echo $newParagrafo; ?></h1>
+    <h3>lunghezza: <?= strlen($newParagrafo)?></h3>
 </body>
 </html>
